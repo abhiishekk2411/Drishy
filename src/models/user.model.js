5b04e3eb6817b11c2a673092c461a2c2b6df8e1f
Abhishek <choudharyabhishek242004@gmail.com>
2026-06-21 13:52:12 +0530

    updated models

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,4 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
@@ -53,5 +53,40 @@ const UserSchema = new Schema(
 )
 
 
+UserSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+
+
+UserSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+UserSchema.methods.generateAccessToken = async function () {
+    return jwt.sign(
+        {
+        _id: this._id,
+        email: this.email,
+        username: this.username,
+        fullname: this.fullname
+    },    
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+    expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }   )
+}
+UserSchema.methods.generateRefreshToken = async function ()
+ {
+    return jwt.sign(
+        {
+        _id: this._id
+    },    
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+    expiresIn: process.env.RERFRESH_TOKEN_EXPIRY
+    }   )
+ }
 
 export const User = mongoose.model("User", UserSchema)

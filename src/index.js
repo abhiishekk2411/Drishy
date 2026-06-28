fef841c3568ed9e06127b9c975c17917cdbaf1ee
Abhishek <choudharyabhishek242004@gmail.com>
2026-06-28 16:07:49 +0530

    added user controller ,user route,updated app.js,updated index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-import dns from 'node:dns'; 
+import dns from 'node:dns';
 dns.setServers(['8.8.8.8', '1.1.1.1']);
-import 'dotenv/config'; 
+import 'dotenv/config';
 import connectDB from "./db/db.js";
+import app from "./app.js";
+
+const PORT = process.env.PORT || 3000;
 
 connectDB();
+
+app.listen(PORT, () => {
+  console.log(`Server chal raha hai: http://localhost:${PORT}`);
+});

73c9700c9f5436877f2afd51d5fd9f40d9ab4ae8
Abhishek <choudharyabhishek242004@gmail.com>
2026-06-30 15:20:40 +0530

    added logoutUser

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,29 @@
+import { User } from "../models/User.model";
+import { ApiError } from "../utils/ApiError";
+import { asyncHandler } from "../utils/asyncHandler";
+
+
+
+export const verifyJWT=asyncHandler(async(req,res,next)=>{
+    try {
+        const token=req.cookie?.accessToken || req.header("Authorization")?.replace("Bearer","")
+    
+        if(!token){
+            throw new ApiError(401,"Unauthorized request")
+        }
+    
+        const decodedToken=jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
+    
+        const user=await User.findById(decodedToken?._id).select(-password -refreshToken)
+    
+        if(!user){
+            throw new ApiError(401,"invalid Access Token")
+        }
+    
+        req.user=user;
+        next()
+    } catch (error) {
+        throw new ApiError(401,error?.message || "invalid access token")
+    }
+
+})
